6adb2ac571698fabede9e1b055a96fe0ff44017a
Natia dokhnadze <dokhnadzenatia70@gmail.com>
2025-06-02 14:34:24 +0200

    added lesson3task4

---
+++ src/main/java/homework3/lesson3task4.java
@@ -0,0 +1,11 @@
+package homework3;
+
+public class lesson3task4 {
+    public static void main(String[] args) {
+        int sum =0;
+        for (int i = 0; i <= 10; i++) {
+            sum = sum + i;
+            System.out.println("Sum of numbers from 0 to 10 is:" + sum);
+        }
+    }
+}
