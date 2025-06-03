c8220c355e6b10c46e5bc81811e2390bfdb0bff3
Natia dokhnadze <dokhnadzenatia70@gmail.com>
2025-06-03 15:49:36 +0200

    added task6

---
+++ src/main/java/homework3/lesson3task6.java
@@ -0,0 +1,18 @@
+package homework3;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class lesson3task6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Please enter a positive number!");
+
+
+
+
+
